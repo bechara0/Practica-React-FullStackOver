a61ddea60c7d809f5586f7176e5487b92da9759d
bechara0 <jjbechara@gmail.com>
2023-07-06 21:21:28 +0000

    funciona array

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,29 +1,31 @@
 import React from "react";
-// import { Content2 } from "./Content2";
 
-export const Content = (props) => {
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+
+export const Content = () => {
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
   return (
     <div>
       <p>
-        {part1.name} {part1.exercises}
+        {parts[0].name} {parts[0].exercises}
       </p>
       <p>
-        {part2.name} {part2.exercises}
+        {parts[1].name} {parts[1].exercises}
       </p>
       <p>
-        {part3.name} {part3.exercises}
+        {parts[2].name} {parts[2].exercises}
       </p>
     </div>
   );
